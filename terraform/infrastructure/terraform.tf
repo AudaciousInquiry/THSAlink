05e29704210d320b8de59af53219328edc95000c
Austin Moody <austin.moody@gmail.com>
2023-09-07 15:31:39 -0400

    Terraform changed name of state files in S3 to remove dev.

--- terraform/infrastructure/terraform.tf
+++ terraform/infrastructure/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "thsa-link-terraform-state"
-    key            = "state/dev.infrastructure.tfstate"
+    key            = "state/infrastructure.tfstate"
     region         = "us-east-1"
     dynamodb_table = "thsa-link-terraform-locks"
     profile        = "starhie"
