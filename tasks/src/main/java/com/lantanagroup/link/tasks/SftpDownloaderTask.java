e58c0bedd84c088523d2e7a2e386024027f8d897
Austin Moody <austin.moody@gmail.com>
2023-09-18 22:00:09 -0400

    Parkland Inventory Import moved to a task to make way to get Lambda working.

---
+++ tasks/src/main/java/com/lantanagroup/link/tasks/SftpDownloaderTask.java
@@ -0,0 +1,81 @@
+package com.lantanagroup.link.tasks;
+
+import com.jcraft.jsch.*;
+import com.lantanagroup.link.tasks.config.SftpDownloaderConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SftpDownloaderTask {
+    private final String username;
+    private final String password;
+    private final String host;
+    private final int port;
+    private final String knownHostsFilePath;
+    private final String knownHostsString;
+    private final String downloadFilePath;
+    private static final Logger logger = LoggerFactory.getLogger(SftpDownloaderTask.class);
+
+    public SftpDownloaderTask(SftpDownloaderConfig config) {
+        username = config.getUsername();
+        password = config.getPassword();
+        host = config.getHost();
+        port = config.getPort();
+        knownHostsFilePath = config.getKnownHosts();
+        downloadFilePath = SetPath(config.getPath(), config.getFileName());
+        knownHostsString = null;
+    }
+
+    public SftpDownloaderTask(SftpDownloaderConfig config, String knownHostsString) {
+        this(config);
+        knownHostsString = knownHostsString;
+    }
+
+    public byte[] download() throws IOException, JSchException, SftpException {
+
+        logger.info("Download call to retrieve {} from {}:{}",
+                downloadFilePath,
+                host,
+                port);
+
+        JSch jSch = new JSch();
+
+        // If knownHostsString contains data use that.  Otherwise, rely on
+        // knownHostsFilePath to read in from a file.
+        if (!(knownHostsString == null) && !knownHostsString.isEmpty() && !knownHostsString.isBlank()) {
+            jSch.setKnownHosts(new ByteArrayInputStream(knownHostsString.getBytes()));
+        } else {
+            jSch.setKnownHosts(knownHostsFilePath);
+        }
+
+        Session session = jSch.getSession(username, host, port);
+        session.setPassword(password);
+        session.connect();
+        logger.info("Session Connected");
+        try {
+            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
+            channel.connect();
+            logger.info("Channel Connected");
+            try (InputStream stream = channel.get(downloadFilePath)) {
+                return stream.readAllBytes();
+            } finally {
+                channel.exit();
+                logger.info("Channel Disconnected");
+            }
+        } finally {
+            session.disconnect();
+            logger.info("Session Disconnected");
+        }
+    }
+
+    private String SetPath(String path, String fileName) {
+        Path filePath = Paths.get(path);
+
+        return String.valueOf(filePath.resolve(fileName));
+    }
+}
