563d1bd9a2769fb37931315f264e70de319b1cdd
Austin Moody <austin.moody@gmail.com>
2023-09-20 09:31:27 -0400

    Added function to Lambda utility for pulling API auth.  Changed ExpungeData to use this.  Altered ExpungeData to have Env variables match other Lambda's where appropriate.

--- link-lambda/src/main/java/RefreshPatientList.java
+++ link-lambda/src/main/java/RefreshPatientList.java
@@ -4,6 +4,20 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class RefreshPatientList implements RequestHandler<Void,String> {
     @Override
     public String handleRequest(Void unused, Context context) {
-        return "";
+        String returnValue = "";
+
+        try {
+
+            // Read API Auth from Secrets
+
+            // Read EPIC Query information from Secrets
+
+            // Read RefreshPatientList configuration from Secrets
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return returnValue;
     }
 }
