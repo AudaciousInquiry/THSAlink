9c348e58bfeff0816873a50e38879408d80d6654
Austin Moody <austin.moody@gmail.com>
2023-09-26 20:31:47 -0400

    API report send now returns Task and kicks off background process.  IReportSender and implmentations return a string from send().

--- core/src/main/java/com/lantanagroup/link/IReportSender.java
+++ core/src/main/java/com/lantanagroup/link/IReportSender.java
@@ -1,16 +1,12 @@
 package com.lantanagroup.link;
 
-import ca.uhn.fhir.context.FhirContext;
-import com.lantanagroup.link.config.api.ApiConfig;
 import com.lantanagroup.link.config.bundler.BundlerConfig;
-import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.DocumentReference;
 import org.hl7.fhir.r4.model.MeasureReport;
-import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface IReportSender {
-  void send(List<MeasureReport> masterMeasureReports, DocumentReference documentReference, HttpServletRequest request, Authentication auth, FhirDataProvider fhirDataProvider, BundlerConfig bundlerConfig) throws Exception;
+  String send(List<MeasureReport> masterMeasureReports, DocumentReference documentReference, HttpServletRequest request, FhirDataProvider fhirDataProvider, BundlerConfig bundlerConfig) throws Exception;
 }
