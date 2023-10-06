526f7c10f25e3b181bedf5e068cc814392910320
Austin Moody <austin.moody@gmail.com>
2023-10-06 16:29:32 -0400

    Blanked out CLI tools in ECS Lambda.  Tweak to Parkland CSV/XLSX lambdas to use a different subnet because 1a is out of IP's.

--- terraform/components/cli-expunge-data.tf
+++ terraform/components/cli-expunge-data.tf
@@ -1,71 +1,71 @@
-module "ecs-task-expunge-data" {
-  source = "../modules/ecs-task-cli"
-
-  application_code = "expunge-data"
-
-  aws_region = var.aws_region
-  aws_profile = var.aws_profile
-  environment = var.environment
-  customer = var.customer
-  project_code = var.project_code
-
-  # TFVARS
-  image_repository = var.docker_image_repository
-  image_name = "thsa-link-cli-expunge-data"
-  image_tag = var.default_docker_tag
-  ecs_task_role = data.terraform_remote_state.infra.outputs.iam-role-arn
-
-  container_environment_file = jsonencode([])
-  container_environment = jsonencode([])
-
-  /*
-  {
-    name: "cli.expunge-data.api-url",
-    value: "http://10.90.90.45:8080/api"
-  }
-  */
-
-  cpu_size = var.default_cpu_size
-  memory_size = var.default_memory_size
-
-  mount_points = jsonencode([{
-    sourceVolume: "cli",
-    containerPath: "/cli",
-    readOnly: false
-  }])
-
-  volume_filesystem_id = data.terraform_remote_state.infra.outputs.file-system.id
-  volume_name = "cli"
-  volume_root_directory = "/cli"
-}
-
-/*
-resource "aws_scheduler_schedule" "expunge-data-schedule" {
-  name = "${var.environment}-${var.customer}-${var.project_code}-expunge-data"
-  group_name = "default"
-
-  flexible_time_window {
-    mode = "OFF"
-  }
-
-  schedule_expression = "rate(45 minutes)"
-
-  target {
-    arn      = data.terraform_remote_state.infra.outputs.ecs_cluster_arn
-    role_arn = aws_iam_role.scheduler-role.arn
-    ecs_parameters {
-      task_definition_arn = module.ecs-task-expunge-data.arn
-      task_count = "1"
-      launch_type = "FARGATE"
-      network_configuration {
-        subnets = var.subnets
-        security_groups = var.security_groups
-        assign_public_ip = false
-      }
-      platform_version = "LATEST"
-      enable_execute_command = false
-      enable_ecs_managed_tags = true
-    }
-  }
-}
-*/
+#module "ecs-task-expunge-data" {
+#  source = "../modules/ecs-task-cli"
+#
+#  application_code = "expunge-data"
+#
+#  aws_region = var.aws_region
+#  aws_profile = var.aws_profile
+#  environment = var.environment
+#  customer = var.customer
+#  project_code = var.project_code
+#
+#  # TFVARS
+#  image_repository = var.docker_image_repository
+#  image_name = "thsa-link-cli-expunge-data"
+#  image_tag = var.default_docker_tag
+#  ecs_task_role = data.terraform_remote_state.infra.outputs.iam-role-arn
+#
+#  container_environment_file = jsonencode([])
+#  container_environment = jsonencode([])
+#
+#  /*
+#  {
+#    name: "cli.expunge-data.api-url",
+#    value: "http://10.90.90.45:8080/api"
+#  }
+#  */
+#
+#  cpu_size = var.default_cpu_size
+#  memory_size = var.default_memory_size
+#
+#  mount_points = jsonencode([{
+#    sourceVolume: "cli",
+#    containerPath: "/cli",
+#    readOnly: false
+#  }])
+#
+#  volume_filesystem_id = data.terraform_remote_state.infra.outputs.file-system.id
+#  volume_name = "cli"
+#  volume_root_directory = "/cli"
+#}
+#
+#/*
+#resource "aws_scheduler_schedule" "expunge-data-schedule" {
+#  name = "${var.environment}-${var.customer}-${var.project_code}-expunge-data"
+#  group_name = "default"
+#
+#  flexible_time_window {
+#    mode = "OFF"
+#  }
+#
+#  schedule_expression = "rate(45 minutes)"
+#
+#  target {
+#    arn      = data.terraform_remote_state.infra.outputs.ecs_cluster_arn
+#    role_arn = aws_iam_role.scheduler-role.arn
+#    ecs_parameters {
+#      task_definition_arn = module.ecs-task-expunge-data.arn
+#      task_count = "1"
+#      launch_type = "FARGATE"
+#      network_configuration {
+#        subnets = var.subnets
+#        security_groups = var.security_groups
+#        assign_public_ip = false
+#      }
+#      platform_version = "LATEST"
+#      enable_execute_command = false
+#      enable_ecs_managed_tags = true
+#    }
+#  }
+#}
+#*/
