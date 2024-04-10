6b5c9ae1abd353d5fbbcefbce1cb6f7fce06c4f0
Austin Moody <austin.moody@gmail.com>
2024-04-10 09:19:16 -0400

    Add new AWS ACM Certificates and update Listener Rules
    Two new ACM certificates have been added for 'test_sanerproject_org_exp_2025' and 'dashboard_sanerproject_org_exp_2025' in the infrastructure setup. Also, an AWS load balancer listener certificate is added for the 'dashboard' with the new certificate. The listener rules for redirecting have been updated to enhance the navigation flow between services.

--- terraform/infrastructure/certificates.tf
+++ terraform/infrastructure/certificates.tf
@@ -17,3 +17,23 @@ resource "aws_acm_certificate" "thsa_sanerproject_org_exp_2024" {
     CreatedBy = "terraform"
   }
 }
+
+resource "aws_acm_certificate" "test_sanerproject_org_exp_2025" {
+  private_key = file("../certs/test_sanerproject_org_expire2025/test_sanerproject_org.private.pem")
+  certificate_body = file("../certs/test_sanerproject_org_expire2025/test_sanerproject_org.certificate.pem")
+  certificate_chain = file("../certs/test_sanerproject_org_expire2025/test_sanerproject_org.chain.pem")
+  tags = {
+    Environment = var.environment,
+    CreatedBy = "terraform"
+  }
+}
+
+resource "aws_acm_certificate" "dashboard_sanerproject_org_exp_2025" {
+  private_key = file("../certs/dashboard_sanerproject_org_expire2025/dashboard_sanerproject_org.private.pem")
+  certificate_body = file("../certs/dashboard_sanerproject_org_expire2025/dashboard_sanerproject_org.certificate.pem")
+  certificate_chain = file("../certs/dashboard_sanerproject_org_expire2025/dashboard_sanerproject_org.chain.pem")
+  tags = {
+    Environment = var.environment,
+    CreatedBy = "terraform"
+  }
+}
